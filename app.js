525cf8f48688dffa0c53a6b09b253c7a04f13c16
Linus-Peng1 <snoopypeng1217@gmail.com>
2021-07-25 04:39:54 +0800

    refactor: modify express / mongoose routes

--- app.js
+++ app.js
@@ -1,63 +1,18 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const ShortUrl = require('./models/shortUrl')
-const shortUrlGenerate = require('./utils/generate_shortUrl')
+
+const routes = require('./routes/index')
+require('./config/mongoose')
 
 const app = express()
-const port = 3000
-const MONGODB_URI = 'mongodb://localhost/url-shortener'
+const port = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayput: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-app.get('/', (req, res) => {
-  res.render('index')
-})
-
-app.post('/shortURL', (req, res) => {
-  const full = req.body.fullURL
-  let short = ''
-
-  ShortUrl.find()
-    .lean()
-    .then(allUrls => {
-
-      // 檢查輸入網址是否已存在
-      existUrl = allUrls.filter(eachUrl => {
-        return eachUrl.full === full
-      })
-      if (existUrl.length === 1) {
-        short = allUrls[0].short
-      } else {
-        short = shortUrlGenerate(5)
-
-        // 檢查 short-URL 是否已存在
-        while (allUrls.some(eachUrl => eachUrl.short === short)) {
-          short = shortUrlGenerate(5)
-        }
-        return ShortUrl.create({ full, short })
-      }
-    })
-    .then(() => res.render('index', { full, short }))
-    .catch(error => console.log(error))
-})
-
-app.get('/:short', (req, res) => {
-  ShortUrl.find({ short: req.params.short })
-    .lean()
-    .then(shortUrl => {
-      if (shortUrl.length === 0) {
-        res.redirect('/')
-      } else {
-        res.redirect(shortUrl[0].full)
-      }
-    })
-})
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
---
+++ routes/modules/home.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const router = express.Router()
+
+const ShortUrl = require('../../models/shortUrl')
+const shortUrlGenerate = require('../../utils/generate_shortUrl')
+
+// home page
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
+router.post('/shortURL', (req, res) => {
+  const full = req.body.fullURL
+  let short = ''
+
+  ShortUrl.find()
+    .lean()
+    .then(allUrls => {
+
+      // 檢查輸入網址是否已存在
+      existUrl = allUrls.filter(eachUrl => {
+        return eachUrl.full === full
+      })
+      if (existUrl.length === 1) {
+        short = allUrls[0].short
+      } else {
+        short = shortUrlGenerate(5)
+
+        // 檢查 short-URL 是否已存在
+        while (allUrls.some(eachUrl => eachUrl.short === short)) {
+          short = shortUrlGenerate(5)
+        }
+        return ShortUrl.create({ full, short })
+      }
+    })
+    .then(() => res.render('index', { full, short }))
+    .catch(error => console.log(error))
+})
+
+router.get('/:short', (req, res) => {
+  ShortUrl.find({ short: req.params.short })
+    .lean()
+    .then(shortUrl => {
+      if (shortUrl.length === 0) {
+        res.redirect('/')
+      } else {
+        res.redirect(shortUrl[0].full)
+      }
+    })
+})
+
+module.exports = router
